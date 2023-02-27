e8906739e0099c9eff2cd6d22e7d743f18a51ae6
BIKADAI <radius.bikadi@gmail.com>
2023-02-27 08:00:58 +0100

    Initialized the project from spring starter and added a README.txt for deployment guide

---
+++ src/main/java/radius/drone/drone/ServletInitializer.java
@@ -0,0 +1,13 @@
+package radius.drone.drone;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DroneApplication.class);
+	}
+
+}
